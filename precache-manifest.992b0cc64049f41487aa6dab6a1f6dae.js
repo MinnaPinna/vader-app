13a4b6f4f8e67b0df654500b8fbe987546ed8ce4
Camilla Svärd Strömgren <camilla.2s@hotmail.se>
2019-05-01 15:12:19 +0200

    Updates

---
+++ precache-manifest.992b0cc64049f41487aa6dab6a1f6dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3967873a5f77fe22cb9c",
+    "url": "/MinnaPinna/vader-app/static/css/main.1992b529.chunk.css"
+  },
+  {
+    "revision": "3967873a5f77fe22cb9c",
+    "url": "/MinnaPinna/vader-app/static/js/main.8fd471fb.chunk.js"
+  },
+  {
+    "revision": "88a27f99922ce2e01e84",
+    "url": "/MinnaPinna/vader-app/static/js/runtime~main.a219a225.js"
+  },
+  {
+    "revision": "8c365d3eaef5940c18f9",
+    "url": "/MinnaPinna/vader-app/static/js/2.f1a28516.chunk.js"
+  },
+  {
+    "revision": "8d93b572a56f8a22b855cb7c0bfeb3f4",
+    "url": "/MinnaPinna/vader-app/static/media/vackertvader.8d93b572.jpg"
+  },
+  {
+    "revision": "defb8057152d920b87ec422f73b97f8c",
+    "url": "/MinnaPinna/vader-app/index.html"
+  }
+];
