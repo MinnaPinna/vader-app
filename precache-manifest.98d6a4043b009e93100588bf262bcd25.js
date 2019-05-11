7848528eba818fe6016b0f12fbb7224356075bbb
Camilla Svärd Strömgren <camilla.2s@hotmail.se>
2019-05-11 11:31:22 +0200

    Updates

---
+++ precache-manifest.98d6a4043b009e93100588bf262bcd25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c8a495c26483f0300e67483415d92a",
+    "url": "/MinnaPinna/vader-app/index.html"
+  },
+  {
+    "revision": "88dfa295e68347af8149",
+    "url": "/MinnaPinna/vader-app/static/css/main.f8208caf.chunk.css"
+  },
+  {
+    "revision": "522d1fc59957de3257b4",
+    "url": "/MinnaPinna/vader-app/static/js/2.65042412.chunk.js"
+  },
+  {
+    "revision": "88dfa295e68347af8149",
+    "url": "/MinnaPinna/vader-app/static/js/main.4a0f39eb.chunk.js"
+  },
+  {
+    "revision": "88a27f99922ce2e01e84",
+    "url": "/MinnaPinna/vader-app/static/js/runtime~main.a219a225.js"
+  },
+  {
+    "revision": "8d93b572a56f8a22b855cb7c0bfeb3f4",
+    "url": "/MinnaPinna/vader-app/static/media/vackertvader.8d93b572.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MinnaPinna/vader-app/precache-manifest.0d791e52d49357804d4016ba56f19141.js"
+  "/MinnaPinna/vader-app/precache-manifest.98d6a4043b009e93100588bf262bcd25.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MinnaPinna/vader-app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MinnaPinna/vader-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
